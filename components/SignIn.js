2019f0bc692f639a763eb8c5cf42550d48ee4354
j1powder <j1powder@yahoo.com>
2023-12-23 06:12:20 -0700

    added login error handling

--- components/SignIn.js
+++ components/SignIn.js
@@ -3,12 +3,14 @@
 
 import { AuthContext } from '@/context/AuthContext';
 import Form from 'react-bootstrap/Form';
-import {Fragment, useContext, useState, useEffect} from 'react'
+import {Fragment, useContext, useState, useEffect, useRef} from 'react'
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import classes from './Gameboard.module.css'
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
+import Toast from 'react-bootstrap/Toast';
+import ToastContainer from 'react-bootstrap/ToastContainer';
 
 
 
@@ -17,6 +19,9 @@ const SignIn = () => {
  const [email, setEmail] = useState('');
  const [password, setPassword] = useState('');
  const {currentUser, signIn } = useContext(AuthContext);
+ const [show, setShow] = useState(false)
+ const emailRef = useRef();
+ const passwordRef = useRef();
  const router = useRouter();
 
  useEffect(()=>{
@@ -38,6 +43,10 @@ const SignIn = () => {
     } catch (error) {
       console.error('Login error:', error.message);
       // Handle error
+      //alert('It Looks like there was an issue logging in')
+      setShow(true)
+      emailRef.current.style.border = "solid red 2px"
+      passwordRef.current.style.border = "solid red 2px"
     }
   };
 
@@ -47,10 +56,10 @@ const SignIn = () => {
                 <Form onSubmit={handleLogin} >
                     <Form.Group>
                         <Form.Label >Email</Form.Label>
-                        <Form.Control type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <Form.Control ref={emailRef} type="email" placeholder="name@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
                         <br/>
                         <Form.Label >Password</Form.Label>
-                        <Form.Control type="password" value={password} onChange={(e) => setPassword(e.target.value)}  />
+                        <Form.Control ref={passwordRef} type="password" value={password} onChange={(e) => setPassword(e.target.value)}  />
                         <br/>
                         <Button variant="light" type="submit">Sign In</Button>
                     </Form.Group>
@@ -59,6 +68,17 @@ const SignIn = () => {
                 <h4 style={{color:"white"}}>Never played before?</h4>
                 <Link href="/register"><Button variant="light">Register here</Button></Link>
                 </Card>
+
+        <ToastContainer position="top-end" className="p-3" style={{ zIndex: 1 }}>
+        <Toast onClose={() => setShow(false)} show={show} delay={3500} autohide bg="warning">
+          <Toast.Header>
+
+            <strong className="me-auto">Login Error</strong>
+            <small className="text-muted"></small>
+          </Toast.Header>
+          <Toast.Body>You might need to register first by clicking <b>register here</b> below</Toast.Body>
+        </Toast>
+      </ToastContainer>
   </Fragment>
 }
 
